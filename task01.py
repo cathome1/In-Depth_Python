c998dbbb6eb1f4ebb81f4e189c13bd472f1d0f35
cathome1 <pridurokman@gmail.com>
2023-09-04 00:48:50 +0300

    fifth homework finished

--- task01.py
+++ task01.py
@@ -4,6 +4,7 @@ def filedata(path: str) -> tuple:
     file_name = path.split("/")[-1].split(".")[0]
     file_type = path.split("/")[-1].split(".")[1]
     file_path = "/".join(path.split("/")[0:-1])
-    return file_path,file_name,file_type
+    return file_path, file_name, file_type
+
 
 print(filedata("/gbfv/thygft/thyjyht/rgh.fgs"))
--- task02.py
+++ task02.py
@@ -5,7 +5,9 @@
 
 def gen_prize(name_list, tax_list, prize_procent_list):
     if len(name_list) == len(tax_list) == len(prize_procent_list):
-        return {name_list[i]:(tax_list[i]/100*float(prize_procent_list[i][:-1])) for i in range(0,len(name_list)) }
-    else: return -1
+        return {name_list[i]: (tax_list[i] / 100 * float(prize_procent_list[i][:-1])) for i in range(0, len(name_list))}
+    else:
+        return -1
 
-print(gen_prize(["Таня", "Вася", "Петя"],[100,300,500],["10%","15.3%","7.9%"]))
+
+print(gen_prize(["Таня", "Вася", "Петя"], [100, 300, 500], ["10%", "15.3%", "7.9%"]))
