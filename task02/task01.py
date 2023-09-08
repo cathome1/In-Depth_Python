30771181f9a5342af0ad638968da37ca0a0beb0d
cathome1 <pridurokman@gmail.com>
2023-09-08 17:08:23 +0300

    second task finished

--- task02/task01.py
+++ task02/task01.py
@@ -3,6 +3,10 @@
 # Для простоты договоримся, что год может быть в диапазоне [1, 9999].
 # Весь период (1 января 1 года - 31 декабря 9999 года) действует Григорианский календарь.
 # Проверку года на високосность вынести в отдельную защищённую функцию.
+
+__all__ = ["is_true_date"]
+
+
 def _is_leap_year(year):
     return (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0))
 
@@ -11,15 +15,15 @@ def is_true_date(str_date: str):
     list_date = str_date.split(".")
     if (len(list_date) != 3):
         return False
-    list_date = list(map(int,list_date))
+    list_date = list(map(int, list_date))
     date, month, year = list_date
-    if(0>date>31 or 0>month>12 or 0>year>9999):
+    if (0 > date > 31 or 0 > month > 12 or 0 > year > 9999):
         return False
-    if(month in (1,3,5,7,8,10,12) and date<=31):
+    if (month in (1, 3, 5, 7, 8, 10, 12) and date <= 31):
         return True
-    elif(month in (4,6,9,11) and date<=30):
+    elif (month in (4, 6, 9, 11) and date <= 30):
         return True
-    elif(month==2 and ((_is_leap_year(year) and date<=29) or (not _is_leap_year(year) and date<=28))):
-            return True
+    elif (month == 2 and ((_is_leap_year(year) and date <= 29) or (not _is_leap_year(year) and date <= 28))):
+        return True
     else:
         return False
