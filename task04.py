ec6e1bdb7d4218485de0ed171b4bcb1e0c8051d0
cathome1 <pridurokman@gmail.com>
2023-10-15 12:27:51 +0300

    fifth task finished

--- task04.py
+++ task04.py
@@ -3,13 +3,16 @@
 import json
 import pickle
 import os
+
+
 def from_json_to_pickle(dir_):
     for file in os.listdir(dir_):
         if file.endswith(".json") and os.path.isfile(file):
             with(open(file, "r", encoding="utf-8") as f_j,
-                 open(file.rstrip("json")+'pickle', "wb") as f_p):
+                 open(file.rstrip("json") + 'pickle', "wb") as f_p):
                 data = json.load(f_j)
-                pickle.dump(data,f_p)
+                pickle.dump(data, f_p)
+
 
 # def pickle_read(file):
 #     with open(file, "rb") as pick:
---
+++ task05.py
@@ -0,0 +1,20 @@
+# Напишите функцию, которая преобразует pickle файл хранящий список словарей в табличный csv файл.
+# Для тестирования возьмите pickle версию файла из предыдущей задачи.
+# Функция должна извлекать ключи словаря для заголовков столбца из переданного файла.
+import csv
+import pickle
+
+
+def from_pickle_to_csv(pickle_file, csv_file="task05.csv"):
+    with(open(pickle_file, "rb") as f_p,
+         open(csv_file, "w", encoding="utf-8", newline="") as f_c):
+        data = pickle.load(f_p)
+        headers = list(data[0].keys())
+        writer = csv.DictWriter(f_c, fieldnames=headers)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    from_pickle_to_csv("task03.pickle")
